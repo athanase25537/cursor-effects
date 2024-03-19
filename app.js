a716d9cb4ef1b738d03bf1dbd952e93a9a66d6f6
ANDRIAMASY Athanase Marc <andriamasy@kali.athanase.org>
2024-03-19 11:58:31 +0300

    mouse move effects color

---
+++ app.js
@@ -0,0 +1,26 @@
+const cursor = document.querySelector('.cursor');
+var timeout;
+
+document.addEventListener('mousemove', (e) => {
+    let x = e.pageX;
+    let y = e.pageY;
+
+    cursor.style.top = y + 'px';
+    cursor.style.left = x + 'px';
+    cursor.style.display = 'block';
+    cursor.style.opacity = 1;
+
+    // Cursor effects on mouse stopped
+    function mouseStopped() {
+        cursor.style.opacity = 0;
+    }
+
+    clearTimeout(timeout);
+    timeout = setTimeout(mouseStopped, 1000);
+})
+
+
+// Cursor effects on mouseout
+document.addEventListener('mouseout', (e) => {
+    cursor.style.display = "none";
+})
